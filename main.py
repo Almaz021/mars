b9cb67c2b1d1a6a8f3dde1ee39b31b47abe4659f
elik <almaz.kzn2005@yandex.ru>
2022-02-19 15:34:09 +0300

    .

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/index/<title>')
+def index(title):
+    param = {}
+    param['title'] = title
+    return render_template('base.html', **param)
+
+
+@app.route('/training/<prof>')
+def training_prof(prof):
+    param = {}
+    param['prof'] = prof
+    param['img1'] = f"{url_for('static', filename='img/1.png')}"
+    param['img2'] = f"{url_for('static', filename='img/2.png')}"
+    return render_template('base.html', **param)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
