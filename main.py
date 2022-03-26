0ab56d943af74b8d6726b567614a276a340e346e
Almaz <90474771+Almaz021@users.noreply.github.com>
2022-03-26 16:51:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,5 +19,12 @@ def training_prof(prof):
     return render_template('base.html', **param)
 
 
+@app.route('/list_prof/<list>')
+def list_prof(list):
+    param = {}
+    param['list'] = list
+    return render_template('index.html', **param)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
